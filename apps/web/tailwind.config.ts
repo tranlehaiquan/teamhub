f7fde221c83cafc6df91eb7c28a04c5239e84809
Q.Tran <quan.tran@metro.digital>
2024-12-21 22:08:08 +0700

    feat(tailwind): add Tailwind CSS configuration and update styles

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,10 @@
+// tailwind config is required for editor support
+import type { Config } from "tailwindcss";
+import sharedConfig from "@repo/tailwind-config";
+
+const config: Pick<Config, "content" | "presets"> = {
+  content: ["./src/app/**/*.tsx", "../../packages/ui/src/**/*.tsx"],
+  presets: [sharedConfig],
+};
+
+export default config;
