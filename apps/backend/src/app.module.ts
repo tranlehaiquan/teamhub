39fd6375be679f24957485f10f0f5cf6bc582131
Q.Tran <quan.tran@metro.digital>
2024-12-22 19:34:34 +0700

    feat(backend): integrate Drizzle ORM and NestJS config module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DrizzleModule } from '@workspace/drizzle-module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    DrizzleModule.register({
+      databaseURL: process.env.DATABASE_URL,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
